b61afda307a053d6767de12204e27635622b8229
Marvin Muster <rabbids@bluewin.ch>
2019-07-17 02:49:36 +0200

    Added pictures with hover effect

--- src/app/components/welcomepage/welcomepage.component.ts
+++ src/app/components/welcomepage/welcomepage.component.ts
@@ -8,6 +8,23 @@ import { Router } from '@angular/router';
 })
 export class WelcomepageComponent implements OnInit {
 
+  readonly rivenBlackWhite = 'assets/img/riven-black-white.png';
+  readonly rivenColored = 'assets/img/RIVEN.png';
+
+  readonly yasuoBlackWhite = 'assets/img/yasuo-black-white.png';
+  readonly yasuoColored = 'assets/img/Yasuo.png';
+
+  readonly ireliaBlackWhite = 'assets/img/irelia-black-white.png';
+  readonly ireliaColored = 'assets/img/Irelia.png';
+
+  readonly mavuinLogo = 'assets/img/Mavuin.png';
+  
+  rivenImg = this.rivenBlackWhite;
+  yasuoImg = this.yasuoBlackWhite;
+  ireliaImg = this.ireliaBlackWhite;
+ 
+
+
   constructor(private _router: Router) {
 
   }
@@ -18,6 +35,34 @@ export class WelcomepageComponent implements OnInit {
 
   public navigateToClips(name: string): void {
     this._router.navigate([`/clips/${name}`]);
+  }
+
+  public enterImage(img: number) {
+    switch (img) {
+      case 1: {
+        this.rivenImg = this.rivenColored;
+        break;
+      }
+      case 2: {
+        this.yasuoImg = this.yasuoColored;
+        break;
+      }
+      case 3: {
+        this.ireliaImg = this.ireliaColored;
+      }
+    }
+  }
+
+  public leaveImage(img: number) {
+    if (img === 1) {
+      this.rivenImg = this.rivenBlackWhite;
+    }
+    if (img === 2) {
+      this.yasuoImg = this.yasuoBlackWhite;
+    }
+    if (img === 3) {
+      this.ireliaImg = this.ireliaBlackWhite
+    }
   }
 
 }
